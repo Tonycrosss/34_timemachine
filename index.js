57ecf3cca43c799c4dcdb2d21beb6b41aebab12e
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-11-21 15:10:53 +0500

    added ;

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var TIMEOUT_IN_SECS = 3 * 60
-var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>'
+var TIMEOUT_IN_SECS = 3 * 60;
+var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>';
 
 function padZero(number){
   return ("00" + String(number)).slice(-2);
@@ -9,36 +9,36 @@ class Timer{
   // IE does not support new style classes yet
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
   constructor(timeout_in_secs){
-    this.initial_timeout_in_secs = timeout_in_secs
+    this.initial_timeout_in_secs = timeout_in_secs;
     this.reset()
   }
   getTimestampInSecs(){
-    var timestampInMilliseconds = new Date().getTime()
+    var timestampInMilliseconds = new Date().getTime();
     return Math.round(timestampInMilliseconds/1000)
   }
   start(){
     if (this.isRunning)
-      return
-    this.timestampOnStart = this.getTimestampInSecs()
+      return;
+    this.timestampOnStart = this.getTimestampInSecs();
     this.isRunning = true
   }
   stop(){
     if (!this.isRunning)
-      return
-    this.timeout_in_secs = this.calculateSecsLeft()
-    this.timestampOnStart = null
+      return;
+    this.timeout_in_secs = this.calculateSecsLeft();
+    this.timestampOnStart = null;
     this.isRunning = false
   }
   reset(timeout_in_secs){
-    this.isRunning = false
-    this.timestampOnStart = null
+    this.isRunning = false;
+    this.timestampOnStart = null;
     this.timeout_in_secs = this.initial_timeout_in_secs
   }
   calculateSecsLeft(){
     if (!this.isRunning)
-      return this.timeout_in_secs
-    var currentTimestamp = this.getTimestampInSecs()
-    var secsGone = currentTimestamp - this.timestampOnStart
+      return this.timeout_in_secs;
+    var currentTimestamp = this.getTimestampInSecs();
+    var secsGone = currentTimestamp - this.timestampOnStart;
     return Math.max(this.timeout_in_secs - secsGone, 0)
   }
 }
@@ -54,27 +54,27 @@ class TimerWidget{
       this.unmount()
 
     // adds HTML tag to current page
-    this.timerContainer = document.createElement('div')
+    this.timerContainer = document.createElement('div');
 
-    this.timerContainer.setAttribute("style", "height: 100px;")
-    this.timerContainer.innerHTML = TEMPLATE
+    this.timerContainer.setAttribute("style", "height: 100px;");
+    this.timerContainer.innerHTML = TEMPLATE;
 
-    rootTag.insertBefore(this.timerContainer, rootTag.firstChild)
+    rootTag.insertBefore(this.timerContainer, rootTag.firstChild);
 
-    this.minutes_element = this.timerContainer.getElementsByClassName('js-timer-minutes')[0]
-    this.seconds_element = this.timerContainer.getElementsByClassName('js-timer-seconds')[0]
+    this.minutes_element = this.timerContainer.getElementsByClassName('js-timer-minutes')[0];
+    this.seconds_element = this.timerContainer.getElementsByClassName('js-timer-seconds')[0];
   }
   update(secsLeft){
     var minutes = Math.floor(secsLeft / 60);
     var seconds = secsLeft - minutes * 60;
 
-    this.minutes_element.innerHTML = padZero(minutes)
-    this.seconds_element.innerHTML = padZero(seconds)
+    this.minutes_element.innerHTML = padZero(minutes);
+    this.seconds_element.innerHTML = padZero(seconds);
   }
   unmount(){
     if (!this.timerContainer)
-      return
-    this.timerContainer.remove()
+      return;
+    this.timerContainer.remove();
     this.timerContainer = this.minutes_element = this.seconds_element = null
   }
 }
